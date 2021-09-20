fdbf7cd38d11767c30bc018604d047682c9fd606
SRC0747 <RjSrc@0747>
2021-09-20 21:49:19 +0530

    added: Measuring and Displaying Feet-Inch Conversion.

--- src/test/java/com/bridgelabz/Feet.java
+++ src/test/java/com/bridgelabz/Feet.java
@@ -1,6 +1,19 @@
 package com.bridgelabz;
 
 public class Feet {
-    public Feet(double v) {
+    private final double value;
+
+    public Feet(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Feet feet = (Feet) obj;
+        return Double.compare(feet.value,value) == 0;
     }
 }
--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -1,9 +1,16 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementTest {
 
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertEquals(feet1,feet2);
+    }
 }
