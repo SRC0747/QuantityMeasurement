ecf05cc57bd896a8e9464e18fac0d58407b2e596
SRC0747 <RjSrc@0747>
2021-09-20 22:36:32 +0530

    added: Measuring and Displaying Feet-Inch Conversion.

--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -13,4 +13,25 @@ class QuantityMeasurementTest {
         Feet feet2 = new Feet(0.0);
         Assertions.assertEquals(feet1,feet2);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assertions.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assertions.assertNotEquals(inch1,inch2);
+    }
 }
